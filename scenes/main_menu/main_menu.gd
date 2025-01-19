bf16845abebf5eaac64f41bb098f0d1c7d38b89f
Shusako <shusako@users.noreply.github.com>
2025-01-19 13:03:10 -0500

    painfully simple main menu

---
+++ scenes/main_menu/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+const FIRST_LEVEL = preload("res://scenes/first_level.tscn")
+const TEST_WORLD_LEVEL = preload("res://scenes/test.tscn")
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_packed(FIRST_LEVEL)
+
+func _on_test_world_button_pressed() -> void:
+	get_tree().change_scene_to_packed(TEST_WORLD_LEVEL)
